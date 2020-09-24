5fcbb25b43194bde414468651f7824cec0750395
andrekisi <andre@kisi.io>
2020-09-24 23:28:05 +0200

    Task.stories

---
+++ src/components/Task.stories.js
@@ -0,0 +1,40 @@
+// src/components/Task.stories.js
+
+import Task from "./Task";
+
+export default {
+  title: "Task",
+  component: Task
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { Task },
+  props: Object.keys(argTypes),
+  template: '<Task v-bind="$props" />'
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: "1",
+    title: "Test Task",
+    state: "TASK_INBOX",
+    updatedAt: new Date(2018, 0, 1, 9, 0)
+  }
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_PINNED"
+  }
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: "TASK_ARCHIVED"
+  }
+};
