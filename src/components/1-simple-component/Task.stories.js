40878f56b5c8716d16338698970300d9c5b13818
Andre Brdoch <andrebrdoch@yahoo.de>
2020-10-04 18:58:05 +0200

    export actions

--- src/components/1-simple-component/Task.stories.js
+++ src/components/1-simple-component/Task.stories.js
@@ -5,16 +5,20 @@ import { action } from "@storybook/addon-actions";
 
 export default {
   title: "1: Simple Component/Task",
-  component: Task
+  component: Task,
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/
+};
+
+export const actionsData = {
+  onPinTask: action("pin-task"),
+  onArchiveTask: action("archive-task")
 };
 
 const Template = (args, { argTypes }) => ({
   components: { Task },
   props: Object.keys(argTypes),
-  methods: {
-    onPinTask: action("on-pin-task"),
-    onArchiveTask: action("on-archive-task")
-  },
+  methods: actionsData,
   template:
     '<Task :task="task" @pin-task="onPinTask" @archive-task="onArchiveTask" />'
 });
