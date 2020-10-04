5fefdaabe09d9d0eaa4937037313ba7d62cc81c3
Andre Brdoch <andrebrdoch@yahoo.de>
2020-10-04 20:09:08 +0200

    TaskList

---
+++ src/components/2-simple-component-build-states/TaskList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="list-items">
+    <template v-if="loading">loading</template>
+    <template v-else-if="isEmpty">empty</template>
+    <template v-else>
+      <Task
+        v-for="task in tasks"
+        :key="task.id"
+        :task="task"
+        v-on="$listeners"
+      />
+    </template>
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+
+export default {
+  components: { Task },
+
+  props: {
+    loading: { type: Boolean, default: false },
+    tasks: { type: Array, default: () => [] }
+  },
+
+  computed: {
+    isEmpty() {
+      return this.tasks.length === 0;
+    }
+  }
+};
+</script>
